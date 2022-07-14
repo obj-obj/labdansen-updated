1a29d7b3c9cc6e249fb98476360f645b97b13c92
[object Object] <object-object@outlook.com>
2022-07-14 09:07:58 -0700

    Formatting

--- LabDansen_1.0/prototypes/sound.lua
+++ LabDansen_1.0/prototypes/sound.lua
@@ -1,9 +1,12 @@
 -- The Lab
 data.raw["lab"]["lab"].working_sound = {
-	sound = {
-		filename = "__LabDansen__/sounds/Lab_Dansen.ogg",
-		volume = 0.5
-	},
-	idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
-	apparent_volume = 2.5,
+    sound = {
+        filename = "__LabDansen__/sounds/Lab_Dansen.ogg",
+        volume = 0.5
+    },
+    idle_sound = {
+        filename = "__base__/sound/idle1.ogg",
+        volume = 0.6
+    },
+    apparent_volume = 2.5
 }
